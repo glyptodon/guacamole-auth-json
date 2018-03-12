054a04e9ae35cbc81ad23de692a36c6f0effeed2
Michael Jumper <mike.jumper@guac-dev.org>
2018-03-11 17:17:35 -0700

    EESM-14: Allow active connections to be joined by ID.

--- src/main/java/org/glyptodon/guacamole/auth/json/connection/ConnectionService.java
+++ src/main/java/org/glyptodon/guacamole/auth/json/connection/ConnectionService.java
@@ -51,9 +51,13 @@ package org.glyptodon.guacamole.auth.json.connection;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 import org.apache.guacamole.GuacamoleException;
 import org.apache.guacamole.GuacamoleServerException;
 import org.apache.guacamole.environment.Environment;
+import org.apache.guacamole.io.GuacamoleReader;
+import org.apache.guacamole.io.GuacamoleWriter;
 import org.apache.guacamole.net.GuacamoleSocket;
 import org.apache.guacamole.net.GuacamoleTunnel;
 import org.apache.guacamole.net.InetGuacamoleSocket;
@@ -64,6 +68,8 @@ import org.apache.guacamole.protocol.ConfiguredGuacamoleSocket;
 import org.apache.guacamole.protocol.GuacamoleClientInformation;
 import org.apache.guacamole.protocol.GuacamoleConfiguration;
 import org.glyptodon.guacamole.auth.json.user.UserData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Service which provides a centralized means of establishing connections,
@@ -75,10 +81,24 @@ import org.glyptodon.guacamole.auth.json.user.UserData;
 public class ConnectionService {
 
     /**
+     * Logger for this class.
+     */
+    private final Logger logger = LoggerFactory.getLogger(ConnectionService.class);
+
+    /**
      * The Guacamole server environment.
      */
     @Inject
     private Environment environment;
+
+    /**
+     * Mapping of the unique IDs of active connections (as specified within the
+     * UserData.Connection object) to the underlying connection ID (as returned
+     * via the Guacamole protocol handshake). Only connections with defined IDs
+     * are tracked here.
+     */
+    private final ConcurrentHashMap<String, String> activeConnections =
+            new ConcurrentHashMap<String, String>();
 
     /**
      * Generates a new GuacamoleConfiguration from the associated protocol and
@@ -94,9 +114,25 @@ public class ConnectionService {
      */
     public GuacamoleConfiguration getConfiguration(UserData.Connection connection) {
 
-        // Create new configuration for given protocol
         GuacamoleConfiguration config = new GuacamoleConfiguration();
-        config.setProtocol(connection.getProtocol());
+
+        // Set connection ID if joining an active connection
+        String primaryConnection = connection.getPrimaryConnection();
+        if (primaryConnection != null) {
+
+            // If no such active connection actually exists, allow things to
+            // fail cleanly by using a non-existent connection ID
+            String id = activeConnections.get(primaryConnection);
+            if (id == null)
+                id = UUID.randomUUID().toString();
+
+            config.setConnectionID(id);
+
+        }
+
+        // Otherwise, require protocol
+        else
+            config.setProtocol(connection.getProtocol());
 
         // Add all parameter name/value pairs
         Map<String, String> parameters = connection.getParameters();
@@ -136,7 +172,7 @@ public class ConnectionService {
         String hostname = proxyConfig.getHostname();
         int port = proxyConfig.getPort();
 
-        GuacamoleSocket socket;
+        final ConfiguredGuacamoleSocket socket;
 
         // Determine socket type based on required encryption method
         switch (proxyConfig.getEncryptionMethod()) {
@@ -163,7 +199,47 @@ public class ConnectionService {
 
         }
 
-        return new SimpleGuacamoleTunnel(socket);
+        // If the current connection is not being tracked (no ID) just return
+        // a normal, non-tracking tunnel
+        final String id = connection.getId();
+        if (id == null)
+            return new SimpleGuacamoleTunnel(socket);
+
+        // If the current connection is intended to be tracked (an ID was
+        // provided), but a connection is already in progress with that ID,
+        // log a warning that the original connection will no longer be tracked
+        final String connectionID = socket.getConnectionID();
+        String activeConnection = activeConnections.put(id, connectionID);
+        if (activeConnection != null)
+            logger.warn("A connection with ID \"{}\" is already in progress, "
+                    + "but another attempt to use this ID has been made. The "
+                    + "original connection will no longer be joinable.", id);
+
+        // Return a tunnel which automatically tracks the active connection
+        return new SimpleGuacamoleTunnel(new GuacamoleSocket() {
+
+            @Override
+            public GuacamoleReader getReader() {
+                return socket.getReader();
+            }
+
+            @Override
+            public GuacamoleWriter getWriter() {
+                return socket.getWriter();
+            }
+
+            @Override
+            public void close() throws GuacamoleException {
+                activeConnections.remove(id, connectionID);
+                socket.close();
+            }
+
+            @Override
+            public boolean isOpen() {
+                return socket.isOpen();
+            }
+
+        });
 
     }
 
